e2d81f456a06ab3f889055b40f7562cdd9fe670c
Kadek Surya Mahardika <kadeksuryam@gmail.com>
2023-12-14 04:38:06 +0800

    add day 4

---
+++ codeforces/100-easy-bootcamp/day4/p42/main.go
@@ -0,0 +1,212 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+func ceil(a, b int) int {
+	res := a / b
+
+	if a%b != 0 {
+		res++
+	}
+
+	return res
+}
+
+func main() {
+	defer out.Flush()
+
+	n := readInts()[0]
+	m := readInts()[0]
+
+	var a []int
+	mx := -int(1e9)
+	for i := 0; i < n; i++ {
+		tmp := readInts()[0]
+
+		a = append(a, tmp)
+		mx = max(mx, tmp)
+	}
+
+	currM := m
+	for i := 0; i < n; i++ {
+		if a[i] != mx {
+			currM -= (mx - a[i])
+		}
+	}
+
+	var minPos, maxPos int
+	if currM <= 0 {
+		minPos = mx
+	} else {
+		minPos = ceil(currM, n) + mx
+	}
+	maxPos = mx + m
+	writeFormat("%d %d\n", minPos, maxPos)
+}
+
+var in = bufio.NewReader(os.Stdin)
+var out = bufio.NewWriter(os.Stdout)
+
+type float = float32
+type double = float64
+type long = int64
+type ulong = uint64
+
+func typeOf(arg interface{}) string {
+	switch reflect.TypeOf(arg).Kind() {
+	case reflect.Int:
+		return "int"
+	case reflect.Int8:
+		return "int8"
+	case reflect.Int16:
+		return "int16"
+	case reflect.Int32:
+		return "int32"
+	case reflect.Int64:
+		return "int64"
+	case reflect.Uint:
+		return "uint"
+	case reflect.Uint8:
+		return "uint8"
+	case reflect.Uint16:
+		return "uint16"
+	case reflect.Uint32:
+		return "uint32"
+	case reflect.Uint64:
+		return "uint64"
+	case reflect.Float32:
+		return "float32"
+	case reflect.Float64:
+		return "float64"
+	case reflect.Complex64:
+		return "complex64"
+	case reflect.Complex128:
+		return "complex128"
+	case reflect.Bool:
+		return "bool"
+	case reflect.String:
+		return "string"
+	case reflect.Chan:
+		return "chan"
+	case reflect.Ptr:
+		return "ptr"
+	case reflect.Uintptr:
+		return "uintptr"
+	case reflect.UnsafePointer:
+		return "unsafeptr"
+	case reflect.Interface:
+		return "interface"
+	case reflect.Array:
+		return "array"
+	case reflect.Slice:
+		return "slice"
+	case reflect.Map:
+		return "map"
+	case reflect.Struct:
+		return "struct"
+	case reflect.Func:
+		return "func"
+	case reflect.Invalid:
+		return "invalid"
+	default:
+		return "nil"
+	}
+}
+
+func _scln() string {
+	ln, _ := in.ReadString('\n')
+	return strings.Trim(ln, " \r\n")
+}
+func _sc(s []string) string {
+	if len(s) == 0 {
+		return _scln()
+	}
+	return s[0]
+}
+
+func readLine() string              { return _scln() }
+func readString() string            { return _scln() }
+func readStrings() []string         { return strings.Split(_scln(), " ") }
+func readBool(s ...string) bool     { t, _ := strconv.ParseBool(_sc(s)); return t }
+func readByte(s ...string) byte     { t, _ := strconv.ParseUint(_sc(s), 10, 8); return byte(t) }
+func readDouble(s ...string) double { t, _ := strconv.ParseFloat(_sc(s), 64); return t }
+func readFloat(s ...string) float   { t, _ := strconv.ParseFloat(_sc(s), 32); return float32(t) }
+func readInt(s ...string) int       { t, _ := strconv.Atoi(_sc(s)); return t }
+func readLong(s ...string) long     { t, _ := strconv.ParseInt(_sc(s), 10, 64); return t }
+func readULong(s ...string) ulong   { t, _ := strconv.ParseUint(_sc(s), 10, 64); return t }
+
+func readBools() []bool {
+	strs := readStrings()
+	arr := make([]bool, len(strs))
+	for i, s := range strs {
+		arr[i] = readBool(s)
+	}
+	return arr
+}
+func readBytes() []byte {
+	strs := readStrings()
+	arr := make([]byte, len(strs))
+	for i, s := range strs {
+		arr[i] = readByte(s)
+	}
+	return arr
+}
+func readDoubles() []double {
+	strs := readStrings()
+	arr := make([]double, len(strs))
+	for i, s := range strs {
+		arr[i] = readDouble(s)
+	}
+	return arr
+}
+func readFloats() []float {
+	strs := readStrings()
+	arr := make([]float, len(strs))
+	for i, s := range strs {
+		arr[i] = readFloat(s)
+	}
+	return arr
+}
+func readInts() []int {
+	strs := readStrings()
+	arr := make([]int, len(strs))
+	for i, s := range strs {
+		arr[i] = readInt(s)
+	}
+	return arr
+}
+func readLongs() []long {
+	strs := readStrings()
+	arr := make([]long, len(strs))
+	for i, s := range strs {
+		arr[i] = readLong(s)
+	}
+	return arr
+}
+func readULongs() []ulong {
+	strs := readStrings()
+	arr := make([]ulong, len(strs))
+	for i, s := range strs {
+		arr[i] = readULong(s)
+	}
+	return arr
+}
+
+func write(arg ...interface{})                 { fmt.Fprint(out, arg...) }
+func writeLine(arg ...interface{})             { fmt.Fprintln(out, arg...) }
+func writeFormat(f string, arg ...interface{}) { fmt.Fprintf(out, f, arg...) }
